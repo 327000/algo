760018b766c253f887afabc0e721a9f6eeb76376
kimnam1 <namil0721@gmail.com>
2022-09-14 23:49:42 +0900

    9/14_2

--- namil/programmers/level2/p2_이진 변환 반복하기.py
+++ namil/programmers/level2/p2_이진 변환 반복하기.py
@@ -1,24 +1,26 @@
+# 채점 및 통과 완료
 def solution(s):
-    temp_numb = s
-    total_zero_count = 0
-    trans_count = 0
-    while temp_numb != '1':
-        temp_trans = transition(temp_numb)  # temp_trans is a list. [transition, zero_count]
-        trans_count += 1
-        temp_numb = temp_trans[0]
-        total_zero_count += temp_trans[1]
-    answer = [trans_count, total_zero_count]
+    total_removing_zero_count = 0
+    transition_count = 0
+    numb_str = s
+    while numb_str != '1':
+        temp = transition(numb_str)
+        numb_str = temp[0]
+        transition_count += 1
+
+        total_removing_zero_count += temp[1]
+
+    answer = [transition_count, total_removing_zero_count]
     return answer
 
-def transition(numb:str):
-    numb_list = numb.split()
-    ans1 = ''
-    zero_count = 0
-    for i in numb_list:
-        if i == "0":
-            zero_count += 1
-        elif i == "1":
-            ans1 = ans1 + i
 
-    ans2 = len(ans1)
-    return [bin(ans2)[2:], zero_count]
+def transition(numb):
+    one_count = 0  # 문자열 길이와 같을 것이다.
+    zero_count = 0
+    for i in numb:
+        if i == '0':
+            zero_count += 1
+        elif i == '1':
+            one_count += 1
+    len_to_bin = bin(one_count)
+    return [len_to_bin[2:], zero_count]  # [이진 변환 결과, 없앤 0 개수]
