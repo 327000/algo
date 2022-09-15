5502d0ebd9d7cdc4d9bb90eb0f9a051dd8f8efdf
kimnam1 <namil0721@gmail.com>
2022-09-16 00:10:26 +0900

    9/15 p1 최소직사각형

---
+++ namil/programmers/level1/p5_최소 직사각형.py
@@ -0,0 +1,21 @@
+def solution(sizes):
+    for i in sizes:
+        w = i[0]
+        h = i[1]
+        if i[0] < i[1]:
+            i[0] = h
+            i[1] = w
+    print(sizes)
+    w_max = 0
+    h_max = 0
+    for i in sizes:
+        if w_max < i[0]:
+            w_max = i[0]
+        if h_max < i[1]:
+            h_max = i[1]
+    return w_max * h_max
+
+
+# 8, 15랑 14, 7하면 14 15
+# 60 50, 70 30, 60 30, 80 40
+#
